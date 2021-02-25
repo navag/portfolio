f58fda4faed4e5f8f3c14b5a808631c2f7220221
navag <38108877+navag@users.noreply.github.com>
2021-02-25 17:32:02 +0530

    first portfolio commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navigation from './Navigation/Navigation'
+
+function App() {
+  return (
+    <div>
+      <Navigation/>
+    </div>
+  );
+}
+
+export default App;
